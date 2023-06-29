54be2043271e6c1125a61ee3c8739e52c436f981
shashankAppinventiv <shashank.kumar@appinventiv.com>
2023-06-29 10:19:36 +0530

    async example

---
+++ async.js
@@ -0,0 +1,10 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+//Async
+
+console.log("Asynchronus Example")
+
+setTimeout(()=>console.log("Hello there"),1000)
+
+console.log("It's executing ")
