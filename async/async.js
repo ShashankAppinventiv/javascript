e5380f38c16cf3e67392f018ff25e89cd3b9a3e1
shashankAppinventiv <shashank.kumar@appinventiv.com>
2023-06-29 10:28:24 +0530

    minor modification

--- async/async.js
+++ async/async.js
@@ -5,6 +5,7 @@
 //non-blocking
 console.log("Asynchronus Example")
 
+setTimeout(()=>alert("This is async behaviour"),3500)
 setTimeout(()=>console.log("Execution complete"),2500)
 
 console.log("It's executing ")
